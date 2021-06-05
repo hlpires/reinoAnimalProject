9f93b2b3854ea86254fe1c0984c6e772266fd467
hlpires <higorlpires@gmail.com>
2021-06-05 01:27:35 -0300

    commit

--- code.js
+++ code.js
@@ -1,12 +1,11 @@
-$('.multiple-items').slick({
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 4
-});
-	
 
-$(document).ready(function(){
-  $('.fotos').slick({
-    
-  });
+
+$('.fotos').slick({
+  
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  adaptiveHeight: true,
+  autoplay:true,
+  autoplaySpeed:2000
 });
