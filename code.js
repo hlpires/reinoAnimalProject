4ba2e14eede61a05421894c769f5401ca163de4a
hlpires <higorlpires@gmail.com>
2021-06-06 01:48:03 -0300

    commit

--- code.js
+++ code.js
@@ -1,3 +1,71 @@
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+
+	function getScrollTopByHref(element) {
+		const id = element.getAttribute('href');
+		return document.querySelector(id).offsetTop;
+	}
+	
+	function scrollToPosition(to) {
+	 
+	  smoothScrollTo(0, to);
+	}
+	
+	function scrollToIdOnClick(event) {
+		event.preventDefault();
+		const to = getScrollTopByHref(event.currentTarget)-120;
+		scrollToPosition(to);
+		
+	}
+
+	
+	function set(){
+		if(jQuery('#servicos').data('clicked')) {
+			scrollToldOnClick();
+		}else if(jQuery('#visite-nos').data('clicked')){
+			scrollToldOnClick1();
+	
+		}else if(jQuery('#bFotos').data('clicked')){
+			scrollToldOnClick2();
+	
+	}
+	
+	}
+set();
+
+
+	menuItems.forEach(item => {
+		item.addEventListener('click', scrollToIdOnClick);
+	});
+	
+
+	function smoothScrollTo(endX, endY, duration) {
+	  const startX = window.scrollX || window.pageXOffset;
+	  const startY = window.scrollY || window.pageYOffset;
+	  const distanceX = endX - startX;
+	  const distanceY = endY - startY;
+	  const startTime = new Date().getTime();
+	
+	  duration = typeof duration !== 'undefined' ? duration : 400;
+	
+	  
+	  const easeInOutQuart = (time, from, distance, duration) => {
+		if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+		return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+	  };
+	
+	  const timer = setInterval(() => {
+		const time = new Date().getTime() - startTime;
+		const newX = easeInOutQuart(time, startX, distanceX, duration);
+		const newY = easeInOutQuart(time, startY, distanceY, duration);
+		if (time >= duration) {
+		  clearInterval(timer);
+		}
+		window.scroll(newX, newY);
+	  }, 1000 / 60); // 60 fps
+	};
+
+
+
 
 var $target = $('#servicos');
 				
@@ -36,3 +104,4 @@ $('#fotos').slick({
   autoplaySpeed:1200,
   speed:1000
 });
+
