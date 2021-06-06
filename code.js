284998b329bf8c05e476e234c96b860f1e39c326
hlpires <higorlpires@gmail.com>
2021-06-05 21:43:35 -0300

    commit

--- code.js
+++ code.js
@@ -1,5 +1,25 @@
 
-
+var $target = $('#servicos');
+				
+				offset = $(window).height() * 1/6+100;
+	
+		function animeScroll() {
+			var documentTop = $(document).scrollTop();
+			
+	
+			$target.each(function(){
+				var itemTop = $('#servicos').offset().top;
+				
+				if (documentTop > itemTop - offset) {
+					$('.servicos').addClass('servicosAnime');
+				
+				} else {
+					
+				}
+			});
+		}
+animeScroll();
+		
 $('#fotos').slick({
   dots:false,
   infinite: true,
