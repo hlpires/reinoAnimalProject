395bbaa9ebd3c779ad947390f03707370907f494
hlpires <higorlpires@gmail.com>
2021-06-13 17:21:27 -0300

    commit

---
+++ code2.js
@@ -0,0 +1,134 @@
+
+
+/*function loadAll() {
+  var x, i;
+  x = document.getElementsByClassName("product");
+  for (i = 0; i < x.length; i++) {
+    w3AddClass(x[i], "show");
+  }
+}*/
+var clock = setInterval(check,200)
+function check(){
+  var checkRacao = document.getElementById('racao').checked;
+  var checkRoupas = document.getElementById('roupas').checked;
+  var checkCasas= document.getElementById('casasCamas').checked;
+  var checkArranhadores = document.getElementById('arranhadores').checked;
+  var checkBrinquedos = document.getElementById('brinquedos').checked;
+  let activeFilters = [];
+
+  if (checkRacao) {
+  
+    
+  }
+  if (checkRoupas) {
+    activeFilters.push('blue');
+    console.log('aperto racao');
+    $('.item1').hide();
+  }
+  if (checkCasas) {
+    activeFilters.push('green');
+    
+  }
+  if (checkArranhadores) {
+    activeFilters.push('purple');
+  
+  }
+  if (checkBrinquedos) {
+    activeFilters.push('purple');
+    
+  }
+}
+
+
+/*
+  //console.log(activeFilters);
+  
+  // get a list of the secondary level filters
+  var checkCircle = document.getElementById('circle').checked;
+  var checkSquare = document.getElementById('square').checked;
+  let secondaryFilters = [];
+  if (checkCircle) {
+    secondaryFilters.push('circle');
+  }
+  if (checkSquare) {
+    secondaryFilters.push('square');
+  }
+  
+  //console.log(secondaryFilters);
+  
+  if (activeFilters.length == 0 && secondaryFilters.length == 0) {
+    // load all
+    filter([""], secondaryFilters);
+  } else {
+    filter(activeFilters, secondaryFilters);
+  }
+  
+}
+
+// Function to sort objects
+function filter (primaryFilters, secondaryFilters) {
+  var x, i;
+  x = document.getElementsByClassName("object");
+  document.getElementById("no-results").style.display = "none";
+  
+  if (primaryFilters.length == 0) {
+    primaryFilters.push("");
+  }
+  
+  for (i = 0; i < x.length; i++) {
+    let matchesFilter = false;
+    w3RemoveClass(x[i], "show");
+    //console.log(x[i].className);
+    for (var p in primaryFilters) {
+      //console.log(primaryFilters[p]);
+      if (secondaryFilters.length > 0 ) {
+        for (var s in secondaryFilters) {
+          //console.log(secondaryFilters[s]);
+            if (x[i].className.indexOf(primaryFilters[p]) > -1 && x[i].className.indexOf(secondaryFilters[s]) > -1) matchesFilter=true;
+        }
+      } else {
+        if (x[i].className.indexOf(primaryFilters[p]) > -1) matchesFilter=true;
+      }
+    }
+    
+    if (matchesFilter) {
+      w3AddClass(x[i], "show");
+      //console.log("Filter added to box #" + i);
+    }
+  }
+  
+  checkForNoMatches();
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+function checkForNoMatches() {
+    var x = document.getElementsByClassName("show");
+    console.log(x.length);
+    if (x.length == 0) {
+      document.getElementById("no-results").style.display = "block";
+    }
+}*/
